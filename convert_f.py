adf8f149430a1ec04def4b19a4675bc82106e6c7
kimcoco <41998428+kimcoco@users.noreply.github.com>
2019-02-07 14:17:20 +0100

    notches updated

--- convert_f.py
+++ convert_f.py
@@ -11,19 +11,22 @@ proc_dir = base_dir+"proc/"
 subjs = ["nc_NEM_12"]
 runs = ["1","2","3","4"]
 
+notches = [16.7, 24, 50, 62, 100, 150, 200]
+breadths = np.array([0.25, 2.0, 1.5, 0.5, 0.5, 0.5, 0.5])
+
 for sub in subjs:
     sub_path = raw_dir+sub+"/"
     for run in runs:
         run_path = sub_path+run+"/"
         #initial reading of 4D raw data
-        rawmeg = mne.io.read_raw_bti(run_path+"c,rfhp1.0Hz",preload=True)
+        rawmeg = mne.io.read_raw_bti(run_path+"c,rfhp1.0Hz",preload=True,rename_channels=False)
         #finding events, adjusting 4D trigger error
-        rawmeg_events = mne.find_events(rawmeg,stim_channel="STI 014",initial_event=True,consecutive=True)
+        rawmeg_events = mne.find_events(rawmeg,stim_channel="TRIGGER",initial_event=True,consecutive=True)
         for i_idx in range(len(rawmeg_events)):
             if rawmeg_events[i_idx,2]>4000:
                 rawmeg_events[i_idx,2]=rawmeg_events[i_idx,2]-4095
         #filtering
-        rawmeg.notch_filter(50,n_jobs="cuda") #cuda is for use of GPU for processing, if not available on your machine, leave out
+        rawmeg.notch_filter(notches,n_jobs="cuda",notch_widths=breadths) #cuda is for use of GPU for processing, if not available on your machine, leave out
         #resampling - maintaining original event time points (!)
         rawmeg,rawmeg_events = rawmeg.resample(200,events=rawmeg_events,n_jobs="cuda") #see above for cuda
         meg_events = rawmeg_events
