ba3d2de573e180a14d9e38aae10d6fca82a3c799
kimcoco <41998428+kimcoco@users.noreply.github.com>
2019-10-18 17:53:50 +0200

    freq progress

--- plot_alpha_diffs.py
+++ plot_alpha_diffs.py
@@ -7,14 +7,14 @@ from scipy import stats
 
 plt.ion() #this keeps plots interactive
 
-base_dir = "C:/Users/kimca/Documents/MEG_analyses/NEMO/"
+base_dir = "D:/NEMO_analyses/"
 proc_dir = base_dir+"proc/"
 subjs = ["nc_NEM_10","nc_NEM_11","nc_NEM_12","nc_NEM_14","nc_NEM_15","nc_NEM_16",
         "nc_NEM_17","nc_NEM_18","nc_NEM_19","nc_NEM_20","nc_NEM_21","nc_NEM_22",
         "nc_NEM_23","nc_NEM_24","nc_NEM_26","nc_NEM_27","nc_NEM_28",
         "nc_NEM_29","nc_NEM_30","nc_NEM_31","nc_NEM_32","nc_NEM_33","nc_NEM_34",
         "nc_NEM_35","nc_NEM_36","nc_NEM_37"]
-#subjs = ["nc_NEM_37"]
+subjs = ["nc_NEM_10"]
 
 for subj in subjs:
     #load resting state measurement of a subjects
@@ -45,9 +45,14 @@ for subj in subjs:
     #separate positive and negative condition epochs
     pos = epo['positive']
     neg = epo['negative']
-    #separate tones r1,r2,s1,s2 for each
 
-
+    #do psd freq analysis on each rest, tonbas, pos/neg, get psds and freqs
+    rest_psds, rest_freqs = mne.time_frequency.psd_multitaper(rest,fmin=1,fmax=40,bandwidth=1)
     ton_psds, ton_freqs = mne.time_frequency.psd_multitaper(tonbas,fmin=1,fmax=40,bandwidth=1)
-    ton_alpha = np.mean(np.mean(ton_psds[...,13:26],axis=2),axis=-1)
-    stats.describe(ton_alpha)
+    pos_psds, pos_freqs = mne.time_frequency.psd_multitaper(pos,fmin = 1,fmax = 40,bandwidth =1)
+    neg_psds, neg_freqs = mne.time_frequency.psd_multitaper(neg,fmin = 1,fmax = 40,bandwidth =1)
+    #get arrays of alpha psds per condition
+    # rest_alpha = np.mean(np.mean(rest_psds[...,13:26],axis=2),axis=-1) # 13 freq values from 7.98 to 13.97
+    # ton_alpha = np.mean(np.mean(ton_psds[...,13:26],axis=2),axis=-1) # 13 freq values from 7.98 to 13.97
+    # pos_alpha = np.mean(np.mean(pos_psds[...,13:26],axis=2),axis=-1) # 13 freq values from 7.98 to 13.97
+    # neg_alpha = np.mean(np.mean(neg_psds[...,13:26],axis=2),axis=-1) # 13 freq values from 7.98 to 13.97
