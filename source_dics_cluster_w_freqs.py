3fccaa389d2fca0ee044a1e80231796198aa517f
kimcoco <41998428+kimcoco@users.noreply.github.com>
2020-04-07 21:33:34 +0200

    ..over freqs

--- source_dics_cluster_w_freqs.py
+++ source_dics_cluster_w_freqs.py
@@ -16,13 +16,42 @@ excluded = {"NEM_30":"DIU11","NEM_32":"NAG83","NEM_33":"FAO18_fa","NEM_37":"EAM6
 # sub_dict = {"NEM_26":"ENR41"}
 freqs = {"theta":list(np.arange(4,7)),"alpha":list(np.arange(8,14)),"beta_low":list(np.arange(17,24)),
          "beta_high":list(np.arange(26,35)),"gamma":(np.arange(35,56)),"gamma_high":(np.arange(65,96))}
-freqs = {"alpha":list(np.arange(8,14))}
+freqs = {"theta":list(np.arange(4,7)),"beta_low":list(np.arange(17,24)),
+         "beta_high":list(np.arange(26,35)),"gamma":(np.arange(35,56)),"gamma_high":(np.arange(65,96))}
 cycles = {"theta":5,"alpha":10,"beta_low":20,"beta_high":30,"gamma":35,"gamma_high":35}
+
+
+for meg,mri in sub_dict.items():
+    # load and prepare the MEG data
+    ton = mne.read_epochs("{dir}nc_{sub}_2_ica-epo.fif".format(dir=meg_dir,sub=meg))
+    epo_exp = mne.read_epochs("{dir}nc_{sub}_exp-epo.fif".format(dir=meg_dir,sub=meg))
+    # load the forward models from each experimental block
+    fwd_ton =  mne.read_forward_solution("{dir}nc_{meg}_2-fwd.fif".format(dir=meg_dir,meg=meg))
+    fwd_exp = mne.read_forward_solution("{dir}nc_{meg}_exp-fwd.fif".format(dir=meg_dir,meg=meg))
+    for freq,vals in freqs.items():
+        # load the csd matrices needed: i.e. for every freq band, get exp, ton, neg and pos (and maybe bas & rest)
+        csd_exp = mne.time_frequency.read_csd("{dir}nc_{meg}-csd_exp_{freq}.h5".format(dir=meg_dir,meg=meg,freq=freq))
+        csd_ton = mne.time_frequency.read_csd("{dir}nc_{meg}-csd_ton_{freq}.h5".format(dir=meg_dir,meg=meg,freq=freq))
+        csd_neg = mne.time_frequency.read_csd("{dir}nc_{meg}-csd_neg_{freq}.h5".format(dir=meg_dir,meg=meg,freq=freq))
+        csd_pos = mne.time_frequency.read_csd("{dir}nc_{meg}-csd_pos_{freq}.h5".format(dir=meg_dir,meg=meg,freq=freq))
+        # make DICS beamformers / filters (common filters) for different freq bands
+        filters_exp = make_dics(epo_exp.info,fwd_exp,csd_exp,pick_ori='max-power',rank=None,inversion='single',weight_norm=None,normalize_fwd=True,real_filter=False)
+        # apply the DICS beamformers to get source Estimates for ton, neg, pos using common filters & save to file
+        stc_ton, freqs_ton = apply_dics_csd(csd_ton,filters_exp)
+        stc_neg, freqs_neg = apply_dics_csd(csd_neg,filters_exp)
+        stc_pos, freqs_pos = apply_dics_csd(csd_pos,filters_exp)
+        # calculate the difference between conditions div. by baseline & save to file
+        stc_diff = (stc_neg - stc_pos) / stc_ton
+        # morph the resulting stcs to fsaverage & save  (to be loaded again and averaged)
+        morph = mne.compute_source_morph(stc_diff,subject_from=mri,subject_to="fsaverage",subjects_dir=mri_dir)
+        stc_fsavg_diff = morph.apply(stc_diff)
+        stc_fsavg_diff.save(fname=meg_dir+"nc_{}_stc_fsavg_diff_F_{}".format(meg,freq))
+
 
 # prepare for source diff permutation t-test
 src = mne.read_source_spaces("{}fsaverage_ico5-src.fif".format(meg_dir))
 connectivity = mne.spatial_src_connectivity(src)
-threshold = 3.850
+threshold = 2.086
 
 for freq,vals in freqs.items():
 
@@ -34,7 +63,7 @@ for freq,vals in freqs.items():
 
     for meg,mri in sub_dict.items():
         # load and prepare the STC data
-        stc_fsavg_diff = mne.read_source_estimate("{}nc_{}_stc_fsavg_diff_{}".format(meg_dir,meg,freq), subject='fsaverage')  ## works without file ending like this (loads both lh and rh)
+        stc_fsavg_diff = mne.read_source_estimate("{}nc_{}_stc_fsavg_diff_F_{}".format(meg_dir,meg,freq), subject='fsaverage')  ## works without file ending like this (loads both lh and rh)
         # collect the individual stcs into lists for averaging later
         all_diff.append(stc_fsavg_diff)
         X_diff.append(stc_fsavg_diff.data.T)
@@ -60,6 +89,6 @@ for freq,vals in freqs.items():
     good_cluster_inds = np.where(cluster_pv < 0.05)[0]
     if len(good_cluster_inds):
         stc_clu_summ = mne.stats.summarize_clusters_stc(clu, p_thresh=0.05, tstep=0.001, tmin=0, subject='fsaverage', vertices=None)
-        stc_clu_summ.plot(subjects_dir=mri_dir,subject='fsaverage',surface='white',hemi='both',time_viewer=True,spacing='ico5',colormap='coolwarm',clim={'kind':'value','pos_lims':(0,0.5,1)})
+        stc_clu_summ.plot(subjects_dir=mri_dir,subject='fsaverage',surface='white',hemi='both',time_viewer=True,spacing='ico5',colormap='coolwarm')
     else:
         print("No sign. clusters found")
