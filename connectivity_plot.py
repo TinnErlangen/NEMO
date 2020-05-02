61d164ee6eb5eb508a64c8f854aea0f054fc90f6
kimcoco <41998428+kimcoco@users.noreply.github.com>
2020-05-02 10:56:15 +0200

    connectivity plot

---
+++ connectivity_plot.py
@@ -0,0 +1,60 @@
+# encoding: utf-8
+"""
+Makes a plot of the grand-average connectivity, contrasted between conditions.
+Both the degree map and the circle diagram are plotted. Author: Marijn van Vliet <w.m.vanvliet@gmail.com>
+"""
+import conpy
+from mayavi import mlab
+
+meg_dir = "D:/NEMO_analyses/proc/"
+mri_dir = "D:/freesurfer/subjects/"
+save_dir = "D:/NEMO_analyses/plots/"
+sub_dict = {"NEM_10":"GIZ04","NEM_11":"WOO07","NEM_12":"TGH11","NEM_14":"FIN23","NEM_15":"KIL13",
+           "NEM_16":"KIO12","NEM_17":"DEN59","NEM_18":"SAG13","NEM_20":"PAG48","NEM_22":"EAM11",
+           "NEM_23":"FOT12","NEM_24":"BII41","NEM_26":"ENR41","NEM_27":"HIU14","NEM_28":"WAL70",
+           "NEM_29":"KIL72","NEM_31":"BLE94","NEM_34":"KER27","NEM_35":"MUN79","NEM_36":"BRA52_fa"}  ## order got corrected
+excluded = {"NEM_30":"DIU11","NEM_32":"NAG83","NEM_33":"FAO18_fa","NEM_37":"EAM67","NEM_19":"ALC81","NEM_21":"WKI71_fa"}
+# sub_dict = {"NEM_26":"ENR41"}
+freqs = {"theta":list(np.arange(4,7)),"alpha":list(np.arange(8,14)),"beta_low":list(np.arange(17,24)),
+         "beta_high":list(np.arange(26,35)),"gamma":(np.arange(35,56)),"gamma_high":(np.arange(65,96))}
+# freqs = {"theta":list(np.arange(4,7)),"alpha":list(np.arange(8,14)),}
+cycles = {"theta":5,"alpha":10,"beta_low":20,"beta_high":30,"gamma":35,"gamma_high":35}
+
+
+freq = "theta"
+
+# Read the pruned connectivity estimates
+con = conpy.read_connectivity('{dir}NEMO_tonbas_vs_rest_contrast_{f}-pruned-avg-connectivity.h5'.format(dir=meg_dir,f=freq))
+con_parc = conpy.read_connectivity('{dir}NEMO_tonbas_vs_rest_contrast_{f}-pruned-label-avg-connectivity.h5'.format(dir=meg_dir,f=freq))
+
+# Plot the degree map
+stc = con.make_stc(summary='degree', weight_by_degree=False)
+fig = mlab.figure(size=(300, 300))
+brain = stc.plot(
+    subject='fsaverage',
+    hemi='both',
+    background='white',
+    foreground='black',
+    time_label='',
+    initial_time=0,
+    smoothing_steps=5,
+    figure=fig,
+)
+brain.scale_data_colormap(0, 1, stc.data.max(), True)
+brain.add_annotation('aparc.a2009s', borders=2)
+
+# Save some views
+mlab.view(0, 90, 450, [0, 0, 0])
+mlab.savefig('{d}tonbas_connectivity_{f}_degree_rh.png'.format(d=save_dir,f=freq), magnification=4)
+mlab.view(180, 90, 450, [0, 0, 0])
+mlab.savefig('{d}tonbas_connectivity_{f}_degree_lh.png'.format(d=save_dir,f=freq), magnification=4)
+mlab.view(180, 0, 450, [0, 10, 0])
+mlab.savefig('{d}tonbas_connectivity_{f}_degree_top.png'.format(d=save_dir,f=freq), magnification=4)
+mlab.view(180, 180, 480, [0, 10, 0])
+mlab.savefig('{d}tonbas_connectivity_{f}_degree_bottom.png'.format(d=save_dir,f=freq), magnification=4)
+
+# Plot the connectivity diagram
+fig, _ = con_parc.plot(title='Parcel-wise Connectivity', facecolor='white',
+                       textcolor='black', node_edgecolor='white',
+                       colormap='plasma_r', vmin=0, show=False)
+fig.savefig('{d}tonbas_connectivity_{f}_squircle.pdf', bbox_inches='tight')
